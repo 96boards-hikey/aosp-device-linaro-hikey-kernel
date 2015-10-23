7124098a14c5904bb43b3190786f03b837ac3413
Vishal Bhoj <vishal.bhoj@linaro.org>
2015-10-23 18:19:13 +0100

    Prebuilt kernel and modules
    Kernel commit: dts/hisilicon: hi6220: Remove dma-coherent from gpu dts node
    wifi module source: https://github.com/96boards/wilink8-wlan_wl18xx
    wifi commit: 3cf1c1774af5eecdc7288588f6b62c2756218a74
    
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>
    Change-Id: I6514ce6732a6ef47bac5db5e1b25cc49e187c88c

---
+++ modules/wifi/wifi.mk
@@ -0,0 +1,7 @@
+PRODUCT_COPY_FILES += \
+        device/linaro/hikey-kernel/modules/wifi/cfg80211.ko:$(PRODUCT_OUT)/system/modules/wifi/cfg80211.ko \
+        device/linaro/hikey-kernel/modules/wifi/compat.ko:$(PRODUCT_OUT)/system/modules/wifi/compat.ko \
+        device/linaro/hikey-kernel/modules/wifi/mac80211.ko:$(PRODUCT_OUT)/system/modules/wifi/mac80211.ko \
+        device/linaro/hikey-kernel/modules/wifi/wl18xx.ko:$(PRODUCT_OUT)/system/modules/wifi/wl18xx.ko \
+        device/linaro/hikey-kernel/modules/wifi/wlcore.ko:$(PRODUCT_OUT)/system/modules/wifi/wlcore.ko \
+        device/linaro/hikey-kernel/modules/wifi/wlcore_sdio.ko:$(PRODUCT_OUT)/system/modules/wifi/wlcore_sdio.ko
